39ed82f54f0bdc6e7a52bbbb343474a195ff5e70
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2023-04-25 16:24:13 -0300

    change base config in vite.config.ts

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/qr-generator/',
+  base: '/qr-coder/',
   plugins: [tsconfigPaths(), svgr(), react()]
 })
